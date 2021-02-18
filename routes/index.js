30b183491d9fa09d3f022c90333def0004f1cd96
Nicole Martindale <nmartind@ucsd.edu>
2021-02-18 10:48:04 -0800

    fixed refresh glitch thing

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,11 @@ var projects = require('../projects.json');
  */
 
 exports.view = function(request, response){
-	  response.render('index', projects);
-	  projects['viewAlt'] = false;
+	projects['viewAlt'] = false;
+	response.render('index', projects);
 };
 
 exports.viewAlt = function(request, response) {
-	response.render('index', projects);
 	projects['viewAlt'] = true;
+	response.render('index', projects);
 };
